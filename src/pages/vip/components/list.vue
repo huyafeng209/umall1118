23181054111e3c90ff900969a97bb2fc9b150e0d
huyafeng <2094677786@qq.com>
2022-05-05 18:17:08 +0800

    添加了vip文件夹

---
+++ src/pages/vip/components/list.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <el-table
+      :data="list"
+      style="width: 100%;margin-bottom: 20px;"
+      row-key="uid"
+      border
+      :tree-props="{children: 'children'}"
+    >
+      <el-table-column prop="uid" label="用户编号" width="120px"></el-table-column>
+      <el-table-column prop="nickname" label="昵称"></el-table-column>
+      <el-table-column prop="phone" label="手机号"></el-table-column>
+      <el-table-column label="状态">
+        <template slot-scope="scope">
+          <el-button type="primary" v-if="scope.row.status===1">启 用</el-button>
+          <el-button type="info" v-else>禁 用</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <!-- scope.row.id直接取到该单元格对象里的id -->
+          <el-button type="primary" @click="edit(scope.row.uid)">编 辑</el-button>
+          <!-- <del-btn @confirm="del(scope.row.id)"></del-btn> -->
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  props: ["list"],
+  computed: {
+    ...mapGetters({}),
+  },
+  methods: {
+    ...mapActions({}),
+    edit(uid) {
+      this.$emit("edit", uid);
+    },
+  },
+  mounted() {},
+};
+</script>
+<style scoped>
+</style>
